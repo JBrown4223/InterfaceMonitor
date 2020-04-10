8edb10af4fd8c4e72f349ff5f857a5e13108d129
Pavan Kumar Kamra <pavankamra96@gmail.com>
2020-04-10 07:42:21 -0400

    Update Logger.cpp

--- Assignment2/Logger/Logger.cpp
+++ Assignment2/Logger/Logger.cpp
@@ -1,1 +1,21 @@
 #include "Logger.h"
+
+Logger::InitializeLog() {
+  
+  
+}
+
+Logger::SetLogLevel(LOG_LEVEL level){
+  
+  
+}
+
+Logger::Log(LOG_LEVEL level, const char *prog, const char *func, int line, const char *message){
+  
+  
+}
+
+Logger::ExitLog(){
+  
+  
+}
