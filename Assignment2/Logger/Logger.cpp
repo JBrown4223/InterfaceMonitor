1606ff4f57adbc146a1b227de53f702e7ea5adc4
Pavan Kumar Kamra <pavankamra96@gmail.com>
2020-04-10 09:57:41 -0400

    Update Logger.cpp

--- Assignment2/Logger/Logger.cpp
+++ Assignment2/Logger/Logger.cpp
@@ -1,9 +1,41 @@
 #include "Logger.h"
-LOG_LEVEL logLevel; 
+#include <arpa/inet.h>
+#include <iostream>
+#include <net/if.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+
+LOG_LEVEL logLevel;
+const int PORT = 1153; 
+const char IP_ADDR[] = "192.168.205.134"; 
 
 Logger::InitializeLog() {
+  int fd, ret, len; 
+  struct sockaddr_in servaddr; 
+
+  if ((fd = socket(AF_INET, SOCK_DGRAM, 0))<0){
+	cout << "Cannot create the socket: " << strerror(errno) << endl; 
+	exit(-1); 
+  }
   
+  memset((char *)&servaddr, 0, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
   
+  if ((ret = inet_pton(AF_INET, IP_ADDR, &servaddr.sin_addr))==0){
+	cout << "No such address: " << strerror(errno) << endl; 
+	close(fd); 
+	exit(-1); 
+  }
+  servaddr.sin_port = htons(PORT); 
+
+ // INCOMPLETE  
 }
 
 Logger::SetLogLevel(LOG_LEVEL level){
