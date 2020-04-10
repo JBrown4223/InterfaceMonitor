80aaae6d2d839fc19a355234ef1ae50561eb9abd
Pavan Kumar Kamra <pavankamra96@gmail.com>
2020-04-10 07:37:08 -0400

    Update Logger.h

--- Assignment2/Logger/Logger.h
+++ Assignment2/Logger/Logger.h
@@ -8,16 +8,21 @@ using namespace std;
 
 const char FILENAME[]="../Server/LogFile.txt";
 
-int InitializeLog();
-void SetLogLevel(LOG_LEVEL level);
-void Log(LOG_LEVEL level, const char *prog, const char *func, int line, const char *message);
-void ExitLog();
-
 typedef enum LOG_LEVEL {
     DEBUG,
     WARNING,
     ERROR,
     CRITICAL
 } LOG_LEVEL;
+
+class Logger {
+    private:
+     
+    public:
+       int InitializeLog();
+       void SetLogLevel(LOG_LEVEL level);
+       void Log(LOG_LEVEL level, const char *prog, const char *func, int line, const char *message);
+       void ExitLog();
+};
 
 #endif//LOGGER_H
